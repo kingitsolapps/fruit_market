1a76407f5fb6378b38e1c323207d6e32e8aa19b5
Muhammad Saqib Mahmood <85228174+saqibjutt@users.noreply.github.com>
2021-07-25 14:10:57 +0500

    init

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,6 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: theme(),
-        //i am testing this.
         // home: SplashScreen(),
         // We use routeName so that we dont need to remember the name
         initialRoute: SplashScreen.routeName,
