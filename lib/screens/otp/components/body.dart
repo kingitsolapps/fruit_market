f98a8ec3cbeff9e6086014803b79d76822583eb4
Muhammad Saqib Mahmood <85228174+saqibjutt@users.noreply.github.com>
2021-08-09 12:27:07 +0500

    init

--- lib/screens/otp/components/body.dart
+++ lib/screens/otp/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_pin_code_fields/flutter_pin_code_fields.dart';
+import 'package:fruit_market/screens/home/home_screen.dart';
 import 'package:get/get.dart';
 import '../../../constants.dart';
 import '../../../size_config.dart';
@@ -19,36 +20,69 @@ class _BodyState extends State<Body> {
   FirebaseAuth _auth = FirebaseAuth.instance;
   String? outputcode;
   String? phoneNumber;
+  String? verId;
+  bool codeSent = false;
   @override
   void initState() {
     super.initState();
     phoneNumber = '+92${widget.phone}';
     print(phoneNumber);
-    final user = _auth.verifyPhoneNumber(
-      phoneNumber: phoneNumber!,
-      verificationCompleted: (PhoneAuthCredential credential) async {
-        await _auth.signInWithCredential(credential);
-      },
-      verificationFailed: (FirebaseAuthException e) {
-        if (e.code == 'invalid-phone-number') {
-          print('The provided phone number is not valid.');
-        }
-      },
-      codeSent: (String verificationId, int? resendToken) async {
-        // Update the UI - wait for the user to enter the SMS code
-        String smsCode = '';
+    verifyPhone();
+    // phoneNumber = '+92${widget.phone}';
+    // print(phoneNumber);
+    // final user = _auth.verifyPhoneNumber(
+    //   phoneNumber: phoneNumber!,
+    //   verificationCompleted: (PhoneAuthCredential credential) async {
+    //     await _auth.signInWithCredential(credential);
+    //   },
+    //   verificationFailed: (FirebaseAuthException e) {
+    //     if (e.code == 'invalid-phone-number') {
+    //       print('The provided phone number is not valid.');
+    //     }
+    //   },
+    //   codeSent: (String verificationId, int? resendToken) async {
+    //     // Update the UI - wait for the user to enter the SMS code
+    //     String smsCode = '';
 
-        // Create a PhoneAuthCredential with the code
-        PhoneAuthCredential credential = PhoneAuthProvider.credential(
-            verificationId: verificationId, smsCode: smsCode);
+    //     // Create a PhoneAuthCredential with the code
+    //     PhoneAuthCredential credential = PhoneAuthProvider.credential(
+    //         verificationId: verificationId, smsCode: smsCode);
 
-        // Sign the user in (or link) with the credential
-        await _auth.signInWithCredential(credential);
-      },
-      codeAutoRetrievalTimeout: (String verificationId) {
-        // Auto-resolution timed out...
-      },
-    );
+    //     // Sign the user in (or link) with the credential
+    //     await _auth.signInWithCredential(credential);
+    //   },
+    //   codeAutoRetrievalTimeout: (String verificationId) {
+    //     // Auto-resolution timed out...
+    //   },
+    // );
+  }
+
+  Future<void> verifyPhone() async {
+    await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: phoneNumber!,
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          await FirebaseAuth.instance.signInWithCredential(credential);
+          final snackBar = SnackBar(content: Text("Login Success"));
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          final snackBar = SnackBar(
+            content: Text("${e.message}"),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        codeSent: (String verificationId, int? resendToken) {
+          setState(() {
+            codeSent = true;
+            verId = verificationId;
+          });
+        },
+        codeAutoRetrievalTimeout: (String verificationId) {
+          setState(() {
+            verId = verificationId;
+          });
+        },
+        timeout: Duration(seconds: 60));
   }
 
   firebaseCall() {
@@ -101,14 +135,14 @@ class _BodyState extends State<Body> {
               ),
               PinCodeFields(
                 length: 6,
-                fieldBorderStyle: FieldBorderStyle.Square,
+                fieldBorderStyle: FieldBorderStyle.Bottom,
                 responsive: true,
                 // fieldHeight: 40.0,
                 // fieldWidth: 40.0,
                 borderWidth: 1.0,
                 // activeBorderColor: Colors.teal,
                 // activeBackgroundColor: Colors.tealAccent,
-                borderRadius: BorderRadius.circular(20.0),
+                // borderRadius: BorderRadius.circular(20.0),
                 keyboardType: TextInputType.number,
                 autoHideKeyboard: false,
                 // fieldBackgroundColor: Colors.lightGreenAccent,
@@ -121,6 +155,7 @@ class _BodyState extends State<Body> {
                   // Your logic with pin code
                   print(output);
                   outputcode = output;
+                  verifyPin(output);
                 },
               ),
               // OtpForm(phone: phone),
@@ -132,8 +167,9 @@ class _BodyState extends State<Body> {
               SizedBox(height: SizeConfig.screenHeight! * 0.1),
               GestureDetector(
                 onTap: () {
-                  firebaseCall();
+                  // firebaseCall();
                   // OTP code resend
+                  verifyPhone();
                 },
                 child: Text(
                   "Resend OTP Code",
@@ -153,8 +189,8 @@ class _BodyState extends State<Body> {
       children: [
         Text("This code will expired in "),
         TweenAnimationBuilder(
-          tween: Tween(begin: 30.0, end: 0.0),
-          duration: Duration(seconds: 30),
+          tween: Tween(begin: 60.0, end: 0.0),
+          duration: Duration(seconds: 60),
           builder: (_, double? value, child) => Text(
             "00:${value!.toStringAsFixed(0)}",
             style: TextStyle(color: kPrimaryColor),
@@ -162,5 +198,25 @@ class _BodyState extends State<Body> {
         ),
       ],
     );
+  }
+
+  void verifyPin(String output) async {
+    PhoneAuthCredential credential =
+        PhoneAuthProvider.credential(verificationId: verId!, smsCode: output);
+    try {
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      final snackBar = SnackBar(
+        content: Text("Login Success"),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      Get.offAll(
+        HomeScreen(),
+      );
+    } on FirebaseAuthException catch (e) {
+      final snackBar = SnackBar(
+        content: Text("${e.message}"),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 }
